e921cf0f187a6b6536d3c0a511340b15e115c0f2
Joaco182004 <87869138+Joaco182004@users.noreply.github.com>
2023-01-03 20:41:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const navbar=document.getElementById('nav')
+const btn_contact=document.getElementById('contact-btn')
+const experience_btn=document.getElementById('experiences-btn')
+const skills_btn=document.getElementById('skills-btn')
+const skills_cont=document.getElementById('skills-content')
+const experiences_cont=document.getElementById('experiences-content')
+navbar.classList.remove('change')
+document.addEventListener("scroll",()=>{
+  
+    if(window.scrollY>70){
+        navbar.classList.add('change')
+    }
+    else{
+        navbar.classList.remove('change')
+    }
+
+})
+
+function onTop(){
+    window.scrollTo(0,0)
+}
+const typed=new Typed('.profession',{
+    strings:['','Desarrollador Web','Diseñador UX/UI','Arquitecto Web','Ingeniero de Datos'],
+    typeSpeed:75,
+    startDelay:300,
+    backSpeed:75,
+    shuffle:false,
+    backDelay:1500,
+    loop:true,
+    loopCount:false,
+    showCursor:true,
+    cursorChar:'|',
+    contentType:'html'
+})
+
+experience_btn.addEventListener('click',()=>{
+    experience_btn.classList.add('active-btn')
+    skills_btn.classList.remove('active-btn')
+    skills_cont.classList.add('desaparecer')
+    experiences_cont.classList.remove('desaparecer')
+})
+skills_btn.addEventListener('click',()=>{
+    experience_btn.classList.remove('active-btn')
+    skills_btn.classList.add('active-btn')
+    skills_cont.classList.remove('desaparecer')
+    experiences_cont.classList.add('desaparecer')
+})
